6e14d65cdde0a077837d93c1cf746da4ac4ee7eb
NUNNAVIJAYAKRISHNA <vijayakrishna.n213@gmail.com>
2025-11-29 22:17:33 +0530

    final try

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json");
+
+// Read service account credentials from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
